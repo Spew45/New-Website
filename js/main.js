bd1d0a6c56dc84a8b2868d1ecd946eaa263c2b4d
Spew45 <103700395+Spew45@users.noreply.github.com>
2022-09-09 14:21:03 -0700

    fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 while (1 < 10) {
-    window.open("https://spew45.github.io/new-website/dump/troll.html")
+    window.open("https://spew45.github.io/new-website/fix.html")
     setTimeout(fn, 1000)
 }
